6f201dcdaaaf31deaf29a7e1f39fc293630061a8
Mariamicheal Raj S <mariamichealrajs@chevron.com>
2020-12-11 23:21:42 +0530

    Add project files.

---
+++ githubpublishsample-mvc-core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace githubpublishsample_mvc_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
